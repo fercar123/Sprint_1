2ad62c15d18d2101b1ae8aa3e079222cf79b7e5c
Fernando <hfernando505@gmail.com>
2016-12-24 14:43:47 -0600

    Agregamos express y nodejs para subir a heroku

---
+++ app.js
@@ -0,0 +1,13 @@
+var express =require('express');
+var app=express();
+var port=process.env.PORT || 8000;
+
+app.use(express.static('public'));
+
+app.get("/",function(req,res){
+	res.sendFile(__dirname+'/index.html');
+});
+
+var server=app.listen(port,function(){
+	console.log('node app funciona bien! y corre en el puerto - ' + port);
+});
